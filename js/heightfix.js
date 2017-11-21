3bf5934442b6f76911572618f22d5aee7fdc1d8a
MhhhxX <max.christoph@web.de>
2017-11-21 23:26:32 +0100

    wait only for first image to load

--- js/heightfix.js
+++ js/heightfix.js
@@ -5,18 +5,9 @@
 
     	$.each(elements, function(i, value) {
     		imgs = $(elements[i]).find("img");
-            
-            // wait until the images are loaded
-            var hasHeight = false;
-            while (!hasHeight) {
-                $.each(imgs, function(j, image){
-                    if ($(image).height() == 0) { 
-                        hasHeight = false;
-                        return false;
-                    }
-                    hasHeight = true;
-                });
-            }
+
+            // wait until first image is loaded
+            while ($(imgs[0]).height() == 0) {}
 
             // start heightfix
             var orientation = $(imgs[0]).attr("data-orientation");
@@ -24,7 +15,7 @@
             var imgCount = imgs.length;
             for (var j = 1; j <= imgCount; j++) {
                 if (orientation == 'p') {
-                    $(imgs[j]).parent().parent().height(imgHeight/(imgCount-1)-imgCount-1);
+                    $(imgs[j]).parent().parent().height(imgHeight/(imgCount-1)-3);
                 } else {
                     $(imgs[j]).parent().parent().height(imgHeight/2);
                 }
